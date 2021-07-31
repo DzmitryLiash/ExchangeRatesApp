8dd8933d7cb204136a3e87ea74164525756668a6
Dzmitry Liashchou <liashchou.d@gmail.com>
2021-07-31 13:37:19 +0200

    delete main storyboard setup AppDelegate.swift setup SceneDelegate.swift add collectionView add snapKit

--- ExchangeRatesApp/ViewController.swift
+++ ExchangeRatesApp/ViewController.swift
@@ -8,12 +8,51 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    lazy var collectionView:UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.itemSize = CGSize(width: 300, height: 150)
+        layout.minimumLineSpacing = 10
+        layout.minimumInteritemSpacing = 10
+        
+        let cv = UICollectionView()
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        cv.dataSource = self
+        cv.delegate = self
+        cv.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "customCell")
+        return cv
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = UIColor.red
+        self.navigationItem.title = "Сurrent exchange rate"
+        self.navigationController?.navigationBar.prefersLargeTitles = true
     }
-
-
 }
+
+extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int { 9 }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "customCell", for: indexPath)
+        cell.backgroundColor = .blue
+        return cell
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int { 1 }
+}
+
+extension ViewController {
+    private func setupSubviews() {
+        view.addSubview(collectionView)
+        
+        
+    }
+}
+
+
+//UITabBarDelegate, UITableViewDataSource
 
